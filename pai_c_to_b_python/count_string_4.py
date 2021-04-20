36891cc3f250349966e41f4493d07cae75615a36
yuki sasaki <69077828+wiskerpaddy@users.noreply.github.com>
2021-04-20 23:12:18 +0900

    Add files via upload

---
+++ pai_c_to_b_python/count_string_4.py
@@ -0,0 +1,11 @@
+input_line = int(input())
+input_line1 = input()
+print(input_line1[input_line  - 1] + " " + input_line1[input_line])
+
+'''
+n = int(input()) - 1
+string = input()
+
+if n + 1 < len(string):
+    print(string[n] + " " + string[n + 1])
+'''
